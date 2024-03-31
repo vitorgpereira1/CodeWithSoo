d0766ab65ca321723d8bf32a9710b8cf37545219
Vitor Pereira <vitorgp14@hotmail.com>
2024-03-31 20:05:26 -0300

    challenge 1

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,28 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
 
-        
+        System.out.println("Digite uma palavra:");
+        String palavra = scanner.nextLine();
+
+        if (isPalindrome(palavra)) {
+            System.out.println("A palavra \"" + palavra + "\" é um palíndromo.");
+        } else {
+            System.out.println("A palavra \"" + palavra + "\" não é um palíndromo.");
+        }
 
         scanner.close();
+    }
+
+    public static boolean isPalindrome(String palavra) {
+        palavra = palavra.replaceAll("\\s+", "").toLowerCase();
+
+        for (int i = 0; i < palavra.length() / 2; i++) {
+            if (palavra.charAt(i) != palavra.charAt(palavra.length() - i - 1)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
